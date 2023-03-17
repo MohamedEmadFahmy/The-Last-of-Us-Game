3df8060fc3ec6c99f35d13a34cbae26ad3e8072e
Omar Yasser <reekamusic5@gmail.com>
2023-03-17 16:56:38 +0200

    test change

--- src/model/characters/Zombie.java
+++ src/model/characters/Zombie.java
@@ -2,10 +2,7 @@ package model.characters;
 
 public class Zombie extends Character {
     private static int ZOMBIES_COUNT = 0;
-    private int currentHp = 40;
-    private int Damage = 10;
-    private String Name;
     public Zombie() {
-        this.Name = "Zombie" + ++ZOMBIES_COUNT;
+        super("Zombie" + ++ZOMBIES_COUNT, 40, 10);
     }
 }
