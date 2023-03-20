bdb5a7aa4cfce35054fe4a243f6c4e7318c93367
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-03-20 23:41:02 +0200

    Update

--- src/model/characters/Medic.java
+++ src/model/characters/Medic.java
@@ -1,14 +1,12 @@
 package model.characters;
 
-import model.collectibles.Vaccine;
+// import model.collectibles.Vaccine;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class Medic extends Hero{
     public Medic(String Name, int maxHp, int attackDmg, int maxActions) {
         super(Name,maxHp,attackDmg,maxActions);
-
-
     }
     public static void main(String[] args) {
     }
