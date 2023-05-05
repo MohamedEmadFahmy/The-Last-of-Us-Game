fa5f8a8ece83dabb7ef82143c18669925c85103c
Omar Yasser <reekamusic5@gmail.com>
2023-05-05 21:27:36 +0300

    use point instead of for loops for getAdj

--- src/model/characters/Character.java
+++ src/model/characters/Character.java
@@ -68,7 +68,8 @@ public abstract class Character {
     public ArrayList<Character> getAdjacentCharacters() {
         ArrayList<Character> list = new ArrayList<Character>();
         int myI = this.getLocation().y;
-        int myJ = this.getLocation().x;
+        int myI = this.getLocation().x;
+        int myJ = 0;
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 15; j++) {
                 if ((i <= myI + 1 && i >= myI - 1) && (j <= myJ + 1 && j >= myJ - 1) && (myI != i || myJ != j)) {
@@ -89,25 +90,9 @@ public abstract class Character {
             c.setCurrentHp(newTargetHP);
             return;
         }
-        onCharacterDeath();
+        // onCharacterDeath();
     }
-
     public void onCharacterDeath() {
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j < 15; j++) {
-                if (Game.map[i][j] instanceof CharacterCell) {
-                    CharacterCell currentCell = (CharacterCell) Game.map[i][j];
-                    Character character = currentCell.getCharacter();
-                    if (character == this) {
-                        currentCell.setCharacter(null);
-                        if (character instanceof Zombie) {
-                            // spawnZombie();
-                            return;
-                        }
-                        Game.heroes.remove(this);
-                    }
-                }
-            }
-        }
+
     }
 }
