c58835a4035b173f4a753a979112ae8549f10843
YoussofEshrah <youssof_eshrah@icloud.com>
2023-05-05 23:31:51 +0300

    Move Done

--- src/model/characters/Hero.java
+++ src/model/characters/Hero.java
@@ -3,8 +3,11 @@ package model.characters;
 import exceptions.InvalidTargetException;
 import exceptions.NotEnoughActionsException;
 import exceptions.NoAvailableResourcesException;
-import model.collectibles.Supply;
-import model.collectibles.Vaccine;
+import model.collectibles.*;
+import model.world.*;
+import engine.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 import java.util.ArrayList;
 
@@ -79,8 +82,117 @@ public abstract class Hero extends Character {
     }
 
     public abstract void useSpecial() throws InvalidTargetException, NoAvailableResourcesException;
-
-    public void move(Direction D) {
+  
+    public boolean checkCharachterCell(Cell targetCell){
+        Character character = ((CharacterCell)targetCell).getCharacter();
+        if(character instanceof Zombie || !(character==null)){
+            return true;
+        }
+        return false;    
     }
+
+
+    // public boolean checkCell(Direction D,int X,int Y){ //checks if the cell the player is trying to move to is available/empty
+    //     if(D==Direction.UP){
+    //         if(Y+1>14){
+    //             return false;
+    //         }
+    //         Cell targetCell=Game.map[Y+1][X];
+    //         if(targetCell instanceof CharacterCell && checkCharachterCell(targetCell)){
+    //             return false;
+    //         }
+    //     } else if(D==Direction.LEFT){
+    //         if(X-1<0){
+    //             return false;
+    //         }
+    //         Cell targetCell=Game.map[Y][X-1];
+    //         if(targetCell instanceof CharacterCell && checkCharachterCell(targetCell)){
+    //             return false;
+    //         }
+    //     } else if(D==Direction.RIGHT){
+    //         if(X+1>14){
+    //             return false;
+    //         }
+    //         Cell targetCell=Game.map[Y][X+1];
+    //         if(targetCell instanceof CharacterCell && checkCharachterCell(targetCell)){
+    //             return false;
+    //         }
+    //     } else if(D==Direction.DOWN){
+    //         if(Y-1<0){
+    //             return false;
+    //         }
+    //         Cell targetCell=Game.map[Y-1][X];
+    //         if(targetCell instanceof CharacterCell && checkCharachterCell(targetCell)){
+    //             return false;
+    //         }   
+    //     }
+    //     return true;
+    // }
+    // public void move(Direction D) {
+    //     int X=(int)this.getLocation().getX();
+    //     int Y=(int)this.getLocation().getY();
+    //     if(checkCell(D, X, Y)){
+
+    //     }
+    //     // if(Game.map[Y][X] instanceof CollectibleCell){
+    //     // }
+    //     }
+
+    public void move(Direction D){  //checks if the cell the player is trying to move to is available/empty
+        int X=(int)this.getLocation().getX();
+        int Y=(int)this.getLocation().getY();    
+        Cell targetCell;
+        if(D==Direction.UP){
+                if(Y+1>14){
+                    return;
+                }
+                Y+=1;
+                targetCell=Game.map[Y][X];
+            } else if(D==Direction.LEFT){
+                if(X-1<0){
+                    return;
+                }
+                X-=1;
+                targetCell=Game.map[Y][X];
+            } else if(D==Direction.RIGHT){
+                if(X+1>14){
+                    return;
+                }
+                X+=1;
+                targetCell=Game.map[Y][X];
+            } else{
+                if(Y-1<0){
+                    return;
+                }
+                Y-=1;
+                targetCell=Game.map[Y][X];
+            }
+            if(targetCell instanceof CharacterCell){
+                if(checkCharachterCell(targetCell)){
+                    return;
+                } 
+            }
+            if(targetCell instanceof CollectibleCell){
+                Collectible collectible=((CollectibleCell)targetCell).getCollectible();
+                if(collectible instanceof Vaccine){
+                    this.vaccineInventory.add((Vaccine)collectible);
+                } else{
+                    this.supplyInventory.add((Supply)collectible);
+                }
+            }
+            if(targetCell instanceof TrapCell){
+                int TrapDamage=((TrapCell)targetCell).getTrapDamage();
+                this.setCurrentHp(this.getCurrentHp()-TrapDamage);
+            }       
+            Point newLocation = new Point(X, Y);
+            this.setLocation(newLocation);
+            for (int i = 0; i < 15; i++) {
+                for (int j = 0; j < 15; j++) {
+                    if( ((j==X) || (j==X+1) || (j==X-1)) && ((i==Y) || (i==Y+1) || (i==Y-1))){
+                        Game.map[i][j].setVisible(true);
+                    }
+                }
+            }
+        }
 
 }
