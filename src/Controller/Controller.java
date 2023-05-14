30e911da5bc29b695c20b2444897e8baa5f8c56b
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-05-15 00:48:30 +0200

    TilePane tets

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,12 +1,17 @@
 package controller;
 
+import javax.swing.GroupLayout.Alignment;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 public class Controller extends Application {
@@ -28,12 +33,15 @@ public class Controller extends Application {
             }
         });
 
-        StackPane root = new StackPane();
+        // GridPane root = new GridPane();
+        TilePane root = new TilePane();
+        root.setAlignment(Pos.CENTER);
         root.getChildren().add(btn);
 
-        Scene scene = new Scene(root, 300, 250);
+        // Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 600, 600);
 
-        primaryStage.setFullScreen(true);
+        // primaryStage.setFullScreen(true);
         primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
