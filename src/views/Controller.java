68e563eea21c2dbbf2391b57448bdb0f4f91d6cf
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-05-16 23:36:23 +0200

    Add start game and exit game buttons Add hover effects to buttons Exit game button exits the game Start game button logs to the console for now

--- src/views/Controller.java
+++ src/views/Controller.java
@@ -9,12 +9,15 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -26,6 +29,7 @@ public class Controller extends Application {
     double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
     double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
     Font font = Font.loadFont(this.getClass().getResourceAsStream("/views/fonts/The Bomb Sound.ttf"), 40);
+    Font font2 = Font.loadFont(this.getClass().getResourceAsStream("/views/fonts/Aka-AcidGR-Compacta.ttf"), 40);
 
     @Override
     public void start(Stage primaryStage) {
@@ -48,11 +52,55 @@ public class Controller extends Application {
         ImagePattern pattern = new ImagePattern(new Image("/views/imgs/bgfinal.png"));
         scene.setFill(pattern);
 
-        Button startGameBtn = new Button("Start game");
+        Button startGameBtn = new Button("START GAME");
+        startGameBtn.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                startGameBtn.getStyleClass().add("hover");
+            }
+        });
+
+        startGameBtn.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                startGameBtn.getStyleClass().remove("hover");
+            }
+        });
+        startGameBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                System.out.println("Switch to character selection scene");
+            }
+        });
 
         startGameBtn.setTranslateY(-screenHeight / 3.5);
-        startGameBtn.setTranslateX(screenWidth / 20);
+        startGameBtn.setTranslateX(screenWidth / 25);
         root.setBottom(startGameBtn);
+
+        Button exitGameBtn = new Button("EXIT GAME");
+        exitGameBtn.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                exitGameBtn.getStyleClass().add("hover");
+            }
+        });
+
+        exitGameBtn.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                exitGameBtn.getStyleClass().remove("hover");
+            }
+        });
+        exitGameBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                primaryStage.close();
+            }
+        });
+
+        exitGameBtn.setTranslateY(screenHeight - (screenHeight / 5));
+        exitGameBtn.setTranslateX(screenWidth / 37);
+        root.setTop(exitGameBtn);
 
         root.setBackground(null);
         primaryStage.setScene(scene);
